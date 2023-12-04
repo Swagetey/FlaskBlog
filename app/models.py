b86aad56e8c36d9eed3ec224a1bafd1f85bdedcf
Pavel <swqgetey@gmail.com>
2023-12-04 21:21:51 +0300

    forth commit

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from . import login_manager
 import jwt
 from flask import current_app
 from datetime import datetime
+import hashlib
+from flask import request
 
 
 class Permission:
@@ -166,6 +168,12 @@ class User(UserMixin, db.Model):
         db.session.commit()
         return True
 
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        url = 'https://secure.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
+
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
@@ -181,3 +189,5 @@ login_manager.anonymous_user = AnonymousUser
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
